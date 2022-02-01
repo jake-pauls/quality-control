15a2aaaceaac25ceefbe5ec5b4a85fa103983f7a
jake-pauls <jakepauls99@gmail.com>
2022-02-01 09:48:25 -0800

    Added custom IDETemplateMacros for clean file header

--- quality-control/AppDelegate.swift
+++ quality-control/AppDelegate.swift
@@ -1,17 +1,11 @@
-//
-//  AppDelegate.swift
 //  quality-control
-//
-//  Created by Jake Pauls on 2022-01-26.
-//
+//  AppDelegate.swift
+//  2022-01-26.
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -30,7 +24,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
--- quality-control/ViewController.swift
+++ quality-control/ViewController.swift
@@ -1,19 +1,12 @@
-//
-//  ViewController.swift
 //  quality-control
-//
-//  Created by Jake Pauls on 2022-01-26.
-//
+//  ViewController.swift
+//  2022-01-26.
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
