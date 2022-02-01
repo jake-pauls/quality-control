b9deb3dd8361815342a2a18e5ad0696339f9d0b6
jake-pauls <jakepauls99@gmail.com>
2022-02-01 09:54:03 -0800

    Initial commit

---
+++ quality-control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quality-control
+//
+//  Created by Jake Pauls on 2022-01-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
